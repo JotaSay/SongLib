c57f9afd3b66177eb192d2526e8b0a5b164a6437
JC Bolanos <jcbolanos98@gmail.com>
2020-02-13 16:41:06 -0500

    adding more controller stuff

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 public class Controller {
+    @FXML
+    ListView<Song> listview;
+
+    private ObservableList<Song> obsList;
+
+    public void Start(){
+        // here we're making our obs list from the arraylist
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,8 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.ListView;
 
 public class Main extends Application {
+
+
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
