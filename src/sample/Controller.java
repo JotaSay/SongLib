7f88ef97fab2a8cbf823544fc8edcb2a137d0ab5
JC Bolanos <jcbolanos98@gmail.com>
2020-02-15 19:38:13 -0500

    Removed fullsong class

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,17 +4,27 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Controller {
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+
+    }
+
+
     @FXML
     ListView<Song> listview;
     @FXML Button okButton, deleteButton, cancelButton;
     @FXML TextField songName;
-
-    @FXML
+    @FXML ChoiceBox<String> choice;
 
     private ObservableList<Song> obsList;
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import view.ListController;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
