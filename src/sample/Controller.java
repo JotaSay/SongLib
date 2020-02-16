efaaf272ce85aa1321737536b30a15f2ca62031e
ihartb <bharu2999@gmail.com>
2020-02-15 20:09:59 -0500

    mostly done delete button

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,18 @@
 package sample;
 
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 
@@ -48,25 +52,24 @@ public class Controller implements Initializable {
 				}
 				
 				Song s = listview.getSelectionModel().getSelectedItem();
+				int index = listview.getSelectionModel().getSelectedIndex();
+				
 				Alert alert = new Alert(AlertType.CONFIRMATION);
-				      //alert.initModality(Modality.NONE);
-				      alert.initOwner(mainStage);
-				      alert.setTitle("Delete");
-				      alert.setHeaderText(
-				           "Are you sure you want to delete the song "+s.getName()+" by "+s.getArtist()+"?");
-
-//				      String content = "Index: " + 
-//				          listView.getSelectionModel()
-//				                   .getSelectedIndex() + 
-//				          "\nValue: " + 
-//				          listView.getSelectionModel()
-//				                   .getSelectedItem();
-//
-//				          alert.setContentText(content);
-				      Optional<ButtonType> res = alert.showAndWait();
+				alert.initOwner(mainStage);
+				alert.setTitle("Delete");
+				alert.setHeaderText(
+						"Are you sure you want to delete the song "+s.getName()+" by "+s.getArtist()+"?");
 				      
-		
-
+				Optional<ButtonType> result = alert.showAndWait();
+				
+				if(result.get() == ButtonType.OK) {
+					obsList.remove(index);
+//					listview.getItems().remove(index);
+					
+				}
+		      
+				alert.close();
+				
 			}
 		});
         
